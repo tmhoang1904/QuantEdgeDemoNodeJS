21a85da60a390ff786d44730264060e1418cd5a8
Hoang Minh Tran <tmhoang1904@gmail.com>
2018-08-16 08:18:07 +0700

    seperate files

--- index.js
+++ index.js
@@ -1,74 +1,20 @@
 var express = require('express');
-var _ = require('lodash');
 var app = express();
-var cors = require('cors')
+var cors = require('cors');
+var DataProvider = require('./DataProvider');
 
 app.use(cors());
 
 var port = process.env.PORT || 8080;
 
-const PAGE_LENGTH = 20;
-
-const COMPANIES = [
-  { name: 'Quant Edge', code: 'QAE.VN' },
-  { name: 'FPT Software', code: 'FPT.VN' },
-  { name: 'NashTech', code: 'NAT.VN' },
-  { name: 'Google', code: 'GOO.VN' },
-  { name: 'Facebook', code: 'FAB.VN' },
-  { name: 'Amazon', code: 'AMA.VN' },
-  { name: 'Apple', code: 'APL.VN' },
-  { name: 'Samsung', code: 'SAM.VN' },
-  { name: 'Oppo', code: 'OPP.VN' }
-];
-
-/**
- * Returns a random number between min (inclusive) and max (exclusive)
- */
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function getRandomInteger(min, max) {
-  return Math.round(getRandomArbitrary(min, max));
-}
-
-function getVolume() {
-  return getRandomInteger(1000, 1000001);
-}
-
-function getCompany() {
-  const index = getRandomInteger(0, COMPANIES.length - 1);
-  return COMPANIES[index];
-}
-
-function getPrice() {
-  return Math.round(Math.random() * 100 * 100) / 100;
-}
-
 app.get('/api/getdata', function(req, res) {
-  let n = 40 + Math.random() * 50;
-  let data = [];
-  for (let i = 0; i < n; i++) {
-    const company = getCompany();
-    const price = getPrice();
-    const volume = getVolume();
-    const value = Math.round(price * volume);
-    const change = 0;
-    const changePercent = 0;
-    data.push({ id: i + 1, company, price, volume, value, change, changePercent });
-  }
-
-  data = _.sortBy(data, [
-    item => {
-      return item.value;
-    }
-  ]);
+  const data = DataProvider.getInitData();
 
   res.send({ data });
 });
 
 var server = app.listen(port, function() {
-  var host = server.address().address;  
+  var host = server.address().address;
 
   console.log('Quant Edge demo app listening at http://%s:%s', host, port);
 });
